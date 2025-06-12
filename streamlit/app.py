2af4da6864ed4c47e9f022acfdab0530bc61a9bf
jairod1 <jairod.programar@gmail.com>
2025-06-12 20:42:32 +0200

    Cambios estéticos 1: Mapa

--- streamlit/app.py
+++ streamlit/app.py
@@ -1080,8 +1080,8 @@ elif opcion == "🏘️ Visión General del Municipio":
         st.markdown("""
         **🗳️ Últimas elecciones:** 28 de mayo de 2023  
         **👤 Próximas elecciones:** 2027  
-        **🏢 Reparto de concejalías 2023:** 12 PP / 5 PSOE / 4 BNG
-        **🏢 Reparto de concejalías 2019:** 15 PP / 5 PSOE / 1 BNG
+        - 🏢 **Reparto de concejalías 2023:** 12 PP / 5 PSOE / 4 BNG
+        - 🗂️ **Reparto de concejalías 2019:** 15 PP / 5 PSOE / 1 BNG
         """)
     
     with col2:
