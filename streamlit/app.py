5b755cb6c8a13ac79cb5476fcfb6a5cadb07ac33
jairod1 <jairod.programar@gmail.com>
2025-06-15 04:28:55 +0200

    Cambios estéticos 2

--- streamlit/app.py
+++ streamlit/app.py
@@ -602,7 +602,7 @@ def procesar_comentarios_individuales_con_sentimientos(df, analizador, top_n=20,
     
 # Header principal
 st.markdown('<h1 class="titulo-sin-linea">🏛️HorizontAI🏛️</h1>', unsafe_allow_html=True)
-st.markdown('<h2 class="titulo-sin-linea">El futuro en tus manos</h2>', unsafe_allow_html=True)
+st.markdown('<h1 class="titulo-sin-linea">El futuro en tus manos</h1>', unsafe_allow_html=True)
 st.caption("<h2 style='text-align: center;'>Sistema de análisis político avanzado con IA emocional</h2>", unsafe_allow_html=True)
 
 # Mostrar estado del analizador
