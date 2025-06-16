1211733bd07d41595e909807e431e27a1ba601e8
jairod1 <jairod.programar@gmail.com>
2025-06-16 03:28:00 +0200

    Cambios estéticos 2

--- streamlit/app.py
+++ streamlit/app.py
@@ -1674,7 +1674,7 @@ elif opcion == "🏘️ Visión General del Municipio":
         <noscript><img src="https://datawrapper.dwcdn.net/9YDQ6/full.png" alt="" /></noscript>
         </div>
         ''',
-        height=720
+        height=800
     )
 
     st.components.v1.html(
