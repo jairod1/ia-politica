dfd399457e49b673039cb9de828f1eba488263a2
jairod1 <jairod.programar@gmail.com>
2025-06-17 07:37:07 +0200

    Cambios estéticos 3: Final

--- streamlit/app.py
+++ streamlit/app.py
@@ -1620,14 +1620,14 @@ elif opcion == "🏘️ Visión General del Municipio":
         **👥 Población:** ~25.000 habitantes  
         **🏘️ Comarca:** O Morrazo  
         **🌍 Provincia:** Pontevedra, Galicia  
-        **⚓ Características:** Villa marinera con tradición naval y comercial
+        **⚓ Características:** Villa marinera con tradición naval
         """)
         
         st.markdown("""
         **🗳️ Últimas elecciones:** 28 de mayo de 2023  
         **👤 Próximas elecciones:** 2027  
-        **🏙️ Reparto concejalías 2023:** 🔵 12 PP / 🔴 5 PSOE / 🌀 4 BNG<br>
-        **📒 Reparto concejalías 2019:** 🔵 15 PP / 🔴 5 PSOE / 🌀 1 BNG
+        **🏙️ Reparto de concejalías en 2023:** 🔵 12 PP / 🔴 5 PSOE / 🌀 4 BNG<br>
+        **📒 Reparto de concejalías en 2019:** 🔵 15 PP / 🔴 5 PSOE / 🌀 1 BNG
         """, unsafe_allow_html=True)
 
     with col2:
