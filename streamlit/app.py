5f62b073deb8a8f66565bb97cdd01ca3ef54580c
jairod1 <jairod.programar@gmail.com>
2025-06-17 07:16:37 +0200

    Cambios estéticos 3: Final

--- streamlit/app.py
+++ streamlit/app.py
@@ -1628,8 +1628,7 @@ if opcion == "🏠 Inicio":
     st.divider()
     st.markdown("### 📚 Más Información 📚")
     st.info("""🔍 **Si quieres conocer más detalles técnicos** sobre el funcionamiento del sistema, ve a la pestaña
-      
-    **📞 Info. Técnica y Contacto**""")      
+    "**📞 Info. Técnica y Contacto**""")      
 
 elif opcion == "🏘️ Visión General del Municipio":
 
