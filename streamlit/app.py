b82a87b7401ae13eb20f8044c4fafb48a2759ccd
jairod1 <jairod.programar@gmail.com>
2025-06-17 07:23:21 +0200

    Cambios estéticos 3: Final

--- streamlit/app.py
+++ streamlit/app.py
@@ -1627,9 +1627,11 @@ if opcion == "🏠 Inicio":
 
     st.divider()
     st.markdown("### 📚 Más Información 📚")
-    st.info("🔍 **Si quieres conocer más detalles técnicos** sobre el funcionamiento del sistema, ve a la pestaña"
-    "**📞 Info. Técnica y Contacto**")
-  
+    st.info("""
+    🔍 **Si quieres conocer más detalles técnicos** sobre el funcionamiento del sistema, ve a la pestaña:
+
+    → **📞 Info. Técnica y Contacto**
+    """)  
 elif opcion == "🏘️ Visión General del Municipio":
 
     st.title("⛪Visión General del Municipio de Marín⛪")
